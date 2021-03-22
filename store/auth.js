1bd32121f491a2fdd7f89a80d7e88f7703c0191e
dominx99 <dominx992@gmail.com>
2021-03-23 00:45:28 +0100

    Change config

--- store/auth.js
+++ store/auth.js
@@ -4,7 +4,7 @@ export const state = () => ({
 
 export const actions = {
   login({}, { email, password }) {
-    return this.$axios.post('api/v1/auth/login', {
+    return this.$axios.post('api/auth/login', {
       email,
       password,
     })
