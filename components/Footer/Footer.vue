8feec3f1ba95522e8d7fe087f86a26ec19e4c789
dominx99 <dominx992@gmail.com>
2021-04-24 14:18:39 +0200

    Expand footer

--- components/Footer/Footer.vue
+++ components/Footer/Footer.vue
@@ -13,37 +13,49 @@
         class="darken-2 text-center"
         color="primary"
       >
-        <v-container>
+        <v-container class="my-4">
           <v-row>
-            <v-col cols="12" md="4">
-              <v-list
-                color="transparent"
-              >
-                <v-list-item class="text-h5">Społeczność</v-list-item>
-                <v-list-item>Facebook</v-list-item>
-                <v-list-item>Instagram</v-list-item>
-                <v-list-item>Youtube</v-list-item>
-              </v-list>
-            </v-col>
-            <v-col cols="12" md="4">
-              <v-list
-                color="transparent"
-              >
-                <v-list-item class="text-h5">Społeczność</v-list-item>
-                <v-list-item>Facebook</v-list-item>
-                <v-list-item>Instagram</v-list-item>
-                <v-list-item>Youtube</v-list-item>
-              </v-list>
-            </v-col>
-            <v-col cols="12" md="4">
-              <v-list
-                color="transparent"
-              >
-                <v-list-item class="text-h5">Społeczność</v-list-item>
-                <v-list-item>Facebook</v-list-item>
-                <v-list-item>Instagram</v-list-item>
-                <v-list-item>Youtube</v-list-item>
-              </v-list>
+            <v-col
+              v-for="(column, index) in columns"
+              :key="index"
+              cols="12"
+              :md="column.flex"
+            >
+              <h5 class="text-h5 text-left mb-2" v-text="column.title"></h5>
+              <div class="d-flex flex-column justify-start align-start">
+                <template
+                  v-for="item in column.items"
+                >
+                  <a
+                    v-if="item.href && item.isExternal"
+                    :href="item.href"
+                    class="d-block white--text text-decoration-none mb-1"
+                    v-text="item.title"
+                    target="_blank"
+                  />
+
+                  <NuxtLink
+                    v-else-if="item.href"
+                    :to="item.href"
+                    class="d-block white--text text-decoration-none mb-1"
+                    v-text="item.title"
+                  />
+
+                  <a
+                    v-else-if="item.hash"
+                    @click="goToElement(item.hash)"
+                    class="d-block white--text text-decoration-none mb-1"
+                    v-text="item.title"
+                    :href="item.hash"
+                  />
+
+                  <p
+                    v-else
+                    class="d-block white--text text-decoration-none mb-1"
+                    v-text="item.title"
+                  />
+                </template>
+              </div>
             </v-col>
           </v-row>
         </v-container>
@@ -74,6 +86,65 @@
 <script>
 export default {
   data: () => ({
+    columns: [
+      {
+        title: 'Firma',
+        flex: 4,
+        items: [
+          {
+            title: 'Ledsystem Jakub Grajnert',
+          },
+          {
+            title: 'NIP: 744-182-61-64',
+          },
+          {
+            title: 'Dąbrowskiego 18D',
+          },
+          {
+            title: '14-200 Iława',
+          },
+        ],
+      },
+      {
+        title: 'Na skróty',
+        flex: 4,
+        items: [
+          {
+            title: 'Realizacje',
+            hash: '#realizations',
+          },
+          {
+            title: 'Oferta',
+            hash: '#offer',
+          },
+          {
+            title: 'Kontakt',
+            hash: '#contact',
+          },
+        ],
+      },
+      {
+        title: 'Media',
+        flex: 4,
+        items: [
+          {
+            isExternal: true,
+            href: 'https://www.facebook.com/ledsystemilawa',
+            title: 'Facebook',
+          },
+          {
+            isExternal: true,
+            href: 'https://www.instagram.com/ledsystem_ledlighting',
+            title: 'Instagram',
+          },
+          {
+            isExternal: true,
+            href: 'https://www.youtube.com/watch?v=C_ddnZqj6o0',
+            title: 'Youtube',
+          },
+        ],
+      },
+    ],
     icons: [
       {
         icon: 'mdi-facebook',
@@ -88,7 +159,21 @@ export default {
         link: 'https://www.youtube.com/watch?v=C_ddnZqj6o0'
       },
     ],
-  })
+  }),
+  methods: {
+    async goToElement(targetElement) {
+      if (this.$route.path != '/') {
+        this.$router.push('/')
+        setTimeout(() => this.$vuetify.goTo(targetElement), 500)
+
+        return
+      }
+
+      this.$vuetify.goTo(targetElement)
+
+      this.extended = false
+    },
+  }
 }
 </script>
 
